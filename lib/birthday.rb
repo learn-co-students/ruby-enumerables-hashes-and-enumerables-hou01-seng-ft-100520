75aad0e85b315beea235b15c6d3c2fcb55660cbc
ionwolf13 <voli.scorp13@gmail.com>
2020-10-01 22:27:47 -0500

    Done.

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -1,9 +1,13 @@
-# birthday_kids = {
-#   "Timmy" => 9, 
-#   "Sarah" => 6, 
-#   "Amanda" => 27
-# }
+birthday_kids = {
+   "Timmy" => 9, 
+  "Sarah" => 6, 
+   "Amanda" => 27
+ }
 
-def happy_birthday(birthday_kids)
-  # add your code snippet here!
+def happy_birthday(array)
+  array.each do |key, value| 
+    puts "Happy Birthday #{key}! You are now #{value} years old!"
+  end
 end
+
+happy_birthday(birthday_kids)
